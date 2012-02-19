e932d0d7f7333648f1c99461eef93de2e380d9a0
Travis McArthur <travis.d.mcarthur@gmail.com>
2012-02-19 11:36:42 -0500

    fixing DC campaign finance data processing

--- dc-campaign-finance/contrib-prep.r
+++ dc-campaign-finance/contrib-prep.r
@@ -11,7 +11,7 @@ unzip( paste(work.dir, "DC_campaign_contribs_99_part12.zip", sep=""), exdir=past
 
 files.to.stack<-list.files(path=paste(work.dir, "raw/", sep=""))
 
-targ.file<-targ.file[grepl("DC_contribs_" targ.file)]
+files.to.stack<-files.to.stack[grepl("DC_contribs_", files.to.stack)]
 
 contribs.df<-read.csv(paste(work.dir, "raw/", files.to.stack[1], sep=""), stringsAsFactors=FALSE)
 
@@ -19,7 +19,10 @@ for ( targ.file in files.to.stack[-1] ) {
 
   contribs.temp.df<-read.csv(paste(work.dir, "raw/", targ.file, sep=""), stringsAsFactors=FALSE)
 
-  contribs.temp.df<-rbind(contribs.df, contribs.temp.df)
+  contribs.df<-rbind(contribs.df, contribs.temp.df)
 
 }
+
+
+
 
