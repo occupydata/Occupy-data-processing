e3a51141689912a644df31ee5aae263c342d8859
Travis McArthur <travis.d.mcarthur@gmail.com>
2012-02-21 21:17:18 -0500

    Start testing on the WebGIS API

--- dc-campaign-finance/contrib-prep.r
+++ dc-campaign-finance/contrib-prep.r
@@ -30,13 +30,47 @@ contribs.to.geocode.df$geocode.id<-1:nrow(contribs.to.geocode.df)
 
 contribs.df<-merge(contribs.df, contribs.to.geocode.df)
 
-contribs.to.geocode.df
-
 geocode.output.ls<-vector("list", length=nrow(contribs.to.geocode.df))
 
+URLencode.vec <- Vectorize(URLencode) 
+# Find a fix for slow operation: 
+# http://r.789695.n4.nabble.com/RFE-vectorize-URLdecode-td901435.html
 
-for ( i in 1:nrow(contribs.df)) {
+contribs.to.geocode.df$api.url<-paste(
+  "http://webgis.usc.edu/Services/Geocode/WebService/GeocoderWebServiceHttpNonParsedAdvanced_V02_96.aspx?streetAddress=",
+  URLencode.vec(contribs.to.geocode.df$Address),
+  "&city=",
+  URLencode.vec(contribs.to.geocode.df$city),
+  "&state=",
+  URLencode.vec(contribs.to.geocode.df$state), 
+  "&zip=",
+  URLencode.vec(contribs.to.geocode.df$Zip),
+  							"&apikey=YOUR_API_KEY_HERE&format=csv&census=true&censusYear=2010&notStore=false&verbose=true&h=u&geom=false&version=2.96",
+  sep=""
+)
 
+
+for ( i in 1:3) {
+
+	geocode.output.ls[[i]]<-
+    tryCatch(read.csv(contribs.to.geocode.df$api.url[i], header=FALSE))
   
 }
 
+  
+  
+  
+  
+# Examples for WebGIS:
+  https://webgis.usc.edu/Services/Geocode/WebService/GeocoderWebServiceHttpNonParsedAdvanced_V02_96.aspx?streetAddress=PO%20Box%20123&city=Beverly%20Hills
+  &state=ca
+  &zip=90210&apikey=demo&format=csv&census=true&censusYear=2010&notStore=false&verbose=true&h=u&geom=false&version=2.96
+  
+  
+  
+  
+  http://webgis.usc.edu/Services/Geocode/WebService/GeocoderWebServiceHttpNonParsedAdvanced_V02_96.aspx?streetAddress=
+  	9355%20Berton%20Way
+  &city=Bevirrly%20Hills&state=ca&zip=90210&apikey=demo&format=csv&census=true&censusYear=2010&notStore=false&verbose=true&sou=true&souatts=city&geom=true&version=2.96
+  
+  
